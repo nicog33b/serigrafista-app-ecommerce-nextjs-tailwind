9c83e59cf67576eea8807e107f63a68c2fd23172
nicog33b <nicolasg99dr@gmail.com>
2024-03-29 22:04:35 -0300

    mutageno ultimate

---
+++ src/app/components/shop/filtroTop.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FiltroTop = ()=> {
+  return (
+
+  <div className=''>
+<div id='filterMenuMobile' className=''></div>
+
+  </div> 
+
+
+  )
+}
+
+export default FiltroTop
